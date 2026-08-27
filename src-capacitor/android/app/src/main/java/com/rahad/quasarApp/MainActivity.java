d18d2720064252850b45bcd0b389e8c184309069
Shakhawat hosen <rahad744698@gmail.com>
2026-08-27 15:31:52 +0300

    Add Capacitor Android mode

---
+++ src-capacitor/android/app/src/main/java/com/rahad/quasarApp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.rahad.quasarApp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src-capacitor/capacitor.config.js
@@ -0,0 +1,6 @@
+const { defineCapacitorConfig } = require('@quasar/app-vite/capacitor');
+
+module.exports = defineCapacitorConfig({
+  appId: 'com.rahad.quasarApp',
+  appName: 'Quasar App'
+});
